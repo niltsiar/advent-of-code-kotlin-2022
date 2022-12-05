2b1a244d4354960ee5ae1925f9be75589243d81c
Borja Quevedo <borjota@gmail.com>
2022-12-05 11:29:49 +0100

    Solutions for day 02

---
+++ src/Day02.kt
@@ -0,0 +1,44 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        val game = mapOf(
+            "A X" to 1 + 3,
+            "A Y" to 2 + 6,
+            "A Z" to 3 + 0,
+            "B X" to 1 + 0,
+            "B Y" to 2 + 3,
+            "B Z" to 3 + 6,
+            "C X" to 1 + 6,
+            "C Y" to 2 + 0,
+            "C Z" to 3 + 3,
+        )
+        return input.sumOf { line ->
+            game[line] ?: 0
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val game = mapOf(
+            "A X" to 3 + 0,
+            "A Y" to 1 + 3,
+            "A Z" to 2 + 6,
+            "B X" to 1 + 0,
+            "B Y" to 2 + 3,
+            "B Z" to 3 + 6,
+            "C X" to 2 + 0,
+            "C Y" to 3 + 3,
+            "C Z" to 1 + 6,
+        )
+        return input.sumOf { line ->
+            game[line] ?: 0
+        }
+    }
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day02_test")
+    check(part1(testInput) == 15)
+    check(part2(testInput) == 12)
+
+    val input = readInput("Day02")
+    println(part1(input))
+    println(part2(input))
+}
